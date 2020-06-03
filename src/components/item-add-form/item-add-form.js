c68a2733fdb8790dbda7402cd411267bf3c63999
Korovko Alexey <korovko@reksoft.ru>
2020-06-03 19:51:35 +0300

    Added opportunity to add items

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,35 @@ import React, { Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.props.onItemAdded(this.state.label)
+  }
+
   render() {
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex"
+            onSubmit={ this.onSubmit }>
+        <input type="text"
+        className="form-control"
+        onChange={ this.onLabelChange }
+        placeholder="What needs to be done" />
         <button
-        className="btn btn-outline-secondary"
-        onClick={ () => this.props.onItemAdded('Hello world') }>
+        className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>  
+      </form>  
     );
   };
 }
